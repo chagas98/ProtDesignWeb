06390815473c036dd6991a8c62a1d7ab9ead8ba5
Samuel Chagas de Assis <samuel.achagas98@gmail.com>
2025-05-09 16:31:55 -0300

    add bar add popups fix post method to read data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import json
 import os
 from Bio.PDB import PDBParser, PPBuilder
@@ -11,6 +11,30 @@ with open("data/stability_lookup.json") as f:
     stability_data = json.load(f)
 
 df = pd.read_csv("data/GH1_mpnn_probs_diff.csv", index_col=0)
+
+print(df)
+convert_aa_names = {
+    "ALA": "A",
+    "ARG": "R",
+    "ASN": "N",
+    "ASP": "D",
+    "CYS": "C",
+    "GLU": "E",
+    "GLN": "Q",
+    "GLY": "G",
+    "HIS": "H",
+    "ILE": "I",
+    "LEU": "L",
+    "LYS": "K",
+    "MET": "M",
+    "PHE": "F",
+    "PRO": "P",
+    "SER": "S",
+    "THR": "T",
+    "TRP": "W",
+    "TYR": "Y",
+    "VAL": "V"
+}
 
 THRESHOLD = 0.0  # valor fictício de threshold para decidir entre estável/instável
 TOPOL_FILE = "GH1_AF_renum.pdb"  # arquivo PDB original
@@ -36,55 +60,55 @@ def start():
 
     mutation_result = None
     sequence = get_sequence_from_pdb(TOPOL_FILE)
-    bar_value = 0.5
-
+    mutation_aa = request.form.get("data-mutation")
+    bar_value = 0
+    redirect_page = None
+    if mutation_aa:
+        # If the mutation is selected, get the corresponding value from the DataFrame
+        mutation_aa = convert_aa_names[mutation_aa] if mutation_aa in convert_aa_names else None
+        print(f"Mutation AA: {mutation_aa}")
+    else:
+        mutation_aa = None
+    
     if request.method == "POST":
-        position = int(request.form["position"])  # Position is an integer
-        mutation_aa = request.form["mutation_aa"]  # Selected amino acid
-        reset = request.form["reset"]
+        aa_name = str(request.form.get("data-mutation"))
+        position = int(request.form.get("data-position"))
+        print(f"Position: {position}, Mutation AA: {mutation_aa}")
 
-        print(f"Received position: {position}, Mutation AA: {mutation_aa}")
-
-        if reset == "1":
-            return render_template("index.html", pdb_file=TOPOL_FILE, mutation_result=None, sequence=sequence)
-
-        if position != 'reset' and mutation_aa:
+        if  mutation_aa:
+            print(f"Mutation AA2: {mutation_aa}")
             # Get the corresponding row (position) and column (mutation amino acid)
-            row = df.loc[position]
+            row = df.iloc[position]
             aa_value = row[mutation_aa]  # Get the value from the DataFrame cell
-
+            print(f"AA Value: {aa_value}")
             # Determine if the mutation is stabilizing or destabilizing
             if aa_value > 0:
                 mutation_result = f"A mutação {position}{mutation_aa} é **ESTABILIZANTE** (ΔΔG={aa_value:.2f})"
                 bar_value = aa_value  # Positive value indicates stabilizing
-                pdb_file = STABLE_FILE
-                redirect_page = "stable"
+                return redirect(url_for("stable", bar_value=bar_value))
+                #return render_template("stable.html", mutation_result=mutation_result, bar_value=bar_value, pdb_file=pdb_file, sequence=sequence, redirect_page=redirect_page)
             elif aa_value < 0:
                 mutation_result = f"A mutação {position}{mutation_aa} é **DESESTABILIZANTE** (ΔΔG={aa_value:.2f})"
                 bar_value = aa_value  # Negative value indicates stabilizing
-                pdb_file = UNSTABLE_FILE
-                redirect_page = "unstable"
+                return redirect(url_for("unstable", bar_value=bar_value))
+                #return render_template("unstable.html", mutation_result=mutation_result, bar_value=bar_value, pdb_file=pdb_file, sequence=sequence, redirect_page=redirect_page)
             else:
                 mutation_result = f"A mutação {position}{mutation_aa} tem ΔΔG de 0, não é nem estabilizante nem destabilizante."
                 bar_value = 0
                 redirect_page = None
 
-            # Normalize the bar value between -1 and 1 for display
-            bar_value = (bar_value + 1) / 2  # Convert value to [0, 1] range for bar
-
-            if redirect_page:
-                return render_template("mutation_process.html", mutation_result=mutation_result, bar_value=bar_value, pdb_file=pdb_file, sequence=sequence, redirect_page=redirect_page)
-        #return render_template("mutation_process.html", pdb_file=pdb_file, mutation_result=mutation_result, sequence=sequence, bar_value=bar_value)
-
-    return render_template("start.html", pdb_file=TOPOL_FILE, sequence=sequence)
+    return render_template("start.html", pdb_file=TOPOL_FILE)
 
 @app.route("/stable")
 def stable():
-    return render_template("stable.html", stable_file=STABLE_FILE)
+    bar_value = request.args.get("bar_value", default=0, type=float)
+    return render_template("stable.html", stable_file=STABLE_FILE, bar_value=bar_value)
+
 
 @app.route("/unstable")
 def unstable():
-    return render_template("unstable.html", unstable_file=UNSTABLE_FILE)
+    bar_value = request.args.get("bar_value", default=0, type=float)
+    return render_template("unstable.html", unstable_file=UNSTABLE_FILE, bar_value=bar_value)
 
 
 if __name__ == "__main__":
